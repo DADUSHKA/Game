d7904ae158855c5bcefe169e0a1355cc091720fd
vicstor <vicstor@bk.ru>
2018-10-13 20:59:16 +0300

    over game two

--- game.rb
+++ game.rb
@@ -1,7 +1,8 @@
 class Game
+  attr_reader :user, :assailant, :bank_user, :bank_assailant, :sum
+
   def initialize
     @deck = CardsOnHands.new
-    @message = GameMessage.new
     @bank_assailant = Bank.new
     @bank_user = Bank.new
   end
@@ -12,121 +13,70 @@ class Game
     @assailant = Assailant.new(@deck)
   end
 
-  def menu_game
-    @message.doing_one(@user.name) if @sum == 20
-
-    if @sum.zero?
-      @assailant.on_hands.clear
-      @message.doing_two(@user.name)
-    end
-  end
-
   def handing_over_cards
     @user.start_game
     @assailant.start_game
-    @message.handing_over_cards(@user.name, @user.vieu_cards, @assailant.name)
     start_bank
   end
+
+  def skip_stroke
+    @user.open_cards
+    @assailant.open_cards
+    validate!
+    opponent_move if @assailant.open_cards.to_i < 17
+  end
+
+  def add_cards
+    @user.add_card
+    opponent_move if @assailant.open_cards.to_i < 17
+  end
+
+  def control_lose
+    user_lose if @assailant.open_cards_valid.to_i <= 21 && @user.open_cards_valid.to_i > 21 ||
+      @assailant.open_cards_valid.to_i > @user.open_cards_valid.to_i &&
+      @assailant.open_cards_valid.to_i <= 21 && @user.open_cards_valid.to_i < 21
+  end
+
+  def control_won
+    user_won if @assailant.open_cards_valid.to_i > 21 && @user.open_cards_valid.to_i <= 21 ||
+      @assailant.open_cards_valid.to_i < @user.open_cards_valid.to_i &&
+      @assailant.open_cards_valid.to_i < 21 && @user.open_cards_valid.to_i <= 21
+  end
+
+  def control_dead_heat
+    dead_heat if @assailant.open_cards_valid.to_i > 21 && @user.open_cards_valid.to_i > 21 ||
+      @assailant.open_cards_valid.to_i == @user.open_cards_valid.to_i
+  end
+
+  private
 
   def start_bank
     @bank_assailant.bank_minus
     @bank_user.bank_minus
     @sum = 20
-    @message.bank(@user.name, @bank_user.bank, @assailant.name, @bank_assailant.bank, @sum)
   end
 
-  def scoring
-    validate!
-    @user.open_cards
-    @assailant.open_cards
-    opponent_move if @assailant.open_cards.to_i < 17 && @assailant.on_hands.size == 2
-    @message.long_2
+  def opponent_move
+    @assailant.add_card
   end
 
-  def add_cards
-    @user.add_card
-    @message.add_cards(@user.view)
-    opponent_move if @assailant.open_cards.to_i < 17
-    open_cards
-  end
-
-  def open_cards
-    message_game
-    @message.open_cards(@user.name, @user.vieu_cards, @user.open_cards_valid,
-                        @assailant.name, @assailant.vieu_cards, @assailant.open_cards_valid)
-    @message.bank(@user.name, @bank_user.bank, @assailant.name, @bank_assailant.bank, @sum)
-  end
-
-  def message_game
-    message_lose
-    message_won
-    message_dead_heat
-  end
-
-  def message_lose
-    you_lose if @assailant.open_cards_valid.to_i <= 21 && @user.open_cards_valid.to_i > 21
-    you_lose if @assailant.open_cards_valid.to_i > @user.open_cards_valid.to_i &&
-      @assailant.open_cards_valid.to_i <= 21 && @user.open_cards_valid.to_i < 21
-  end
-
-  def message_won
-    you_won if @assailant.open_cards_valid.to_i > 21 && @user.open_cards_valid.to_i <= 21
-    you_won if @assailant.open_cards_valid.to_i < @user.open_cards_valid.to_i &&
-      @assailant.open_cards_valid.to_i < 21 && @user.open_cards_valid.to_i <= 21
-  end
-
-  def message_dead_heat
-    dead_heat if @assailant.open_cards_valid.to_i > 21 && @user.open_cards_valid.to_i > 21
-    dead_heat if @assailant.open_cards_valid.to_i == @user.open_cards_valid.to_i
-  end
-
-  def you_lose
-    @message.you_lose
+  def user_lose
     @bank_user.bank
     @bank_assailant.bank_plus_two
     @sum = 0
   end
 
-  def you_won
-    @message.you_won
+  def user_won
     @bank_user.bank_plus_two
     @bank_assailant.bank
     @sum = 0
   end
 
   def dead_heat
-    @message.dead_heat
     @bank_user.bank_plus_one
     @bank_assailant.bank_plus_one
     @sum = 0
   end
-
-  def opponent_move
-    @message.opponent_move(@assailant.name)
-    @assailant.add_card
-  end
-
-  def welcome
-    @message.welcome
-  end
-
-  def error_message(mes)
-    @message.error_message(mes)
-  end
-
-  def command
-    @message.command
-  end
-
-  def wrong
-    @message.wrong
-  end
-
-  def pause
-    @message.pause
-  end
-
-  private
 
   def validate!
     raise "#{@user.name} не может пропустить ход!" if @assailant.open_cards.to_i > 17
--- terminal.rb
+++ terminal.rb
@@ -7,46 +7,169 @@ class TerminalInterface
   private
 
   def welcome
-    @game.welcome
+    welcome_message
     @name = gets.capitalize.chomp
     validate!
     @game.create_players(@name)
-    @game.handing_over_cards
-    control_game
+    play_game
   rescue StandardError => e
-    @game.error_message(e.message)
+    error_message(e.message)
     retry
+  end
+
+  def play_game
+    @game.handing_over_cards
+    handing_over_cards
+    bank
+    control_game
+  end
+
+  def skip_stroke
+    opponent_move if @game.assailant.open_cards.to_i < 17
+    @game.skip_stroke
+    bank
+  end
+
+  def add_card
+    opponent_move if @game.assailant.open_cards.to_i < 17
+    @game.add_cards
+    add_cards
+    open_cards
+  end
+
+  def open_cards
+    check_game
+    game_results
+    bank
   end
 
   def control_game
     catch :exit do
       loop do
         puts '---------------'
-        @game.menu_game
-        first_command = @game.command
+        menu_game
+        first_command = command
         throw :exit if first_command == '9'
         case first_command
         when '1'
-          option(@game.scoring)
+          option(skip_stroke)
         when '2'
-          option(@game.add_cards)
+          option(add_card)
         when '3'
-          option(@game.open_cards)
+          option(open_cards)
         when '4'
-          option(@game.handing_over_cards)
+          option(play_game)
         else
-          option(@game.wrong)
+          option(wrong)
         end
       rescue StandardError => e
-        @game.error_message(e.message)
+        error_message(e.message)
         retry
       end
     end
   end
 
+  def check_game
+    you_lose if @game.control_lose
+    you_won if @game.control_won
+    dead_heat if @game.control_dead_heat
+  end
+
   def option(menu_opt)
     menu_opt
-    @game.pause
+    pause
+  end
+
+  def welcome_message
+    puts "Приветствую Вас в игре BlackJack! \n
+        Чтобы начать игру введите Ваше имя:"
+  end
+
+  def menu_game
+    if @game.sum == 20
+      puts "  Ваши #{@game.user.name} действия :
+              1 - Пропустить ход
+              2 - Добавить карту
+              3 - Открыть карты"
+    else
+      puts "  Ваши #{@game.user.name} действия :
+              4 - Продолжить игру
+              9 - Выйти"
+    end
+  end
+
+  def game_results
+    puts '             Итоги игры:'
+    puts "      :: #{@game.user.name}: #{@game.user.vieu_cards}  - очки: #{@game.user.open_cards_valid}
+      :: #{@game.assailant.name}: #{@game.assailant.vieu_cards}  - очки: #{@game.assailant.open_cards_valid} "
+  end
+
+  def bank
+    puts " Счет #{@game.user.name}:₽#{@game.bank_user.bank}  ::  Cчет #{@game.assailant.name}:₽#{@game.bank_assailant.bank}"
+    puts "          На кону: ₽#{@game.sum}"
+  end
+
+  def you_lose
+    puts '          ☹ Вы проиграли ☹'
+  end
+
+  def you_won
+    puts '          ☺  Вы выиграли ☺'
+  end
+
+  def dead_heat
+    puts '          ☺  Ничья ☺'
+  end
+
+  def opponent_move
+    puts "  -->> Ход #{@game.assailant.name}--> ☺ -->  "
+    long_2
+  end
+
+  def handing_over_cards
+    puts " #{@game.user.name}, игра началась!"
+    long_1
+    puts '        -->> Раздача карт-->>'
+    long_3
+    puts
+    puts "          ! Карты розданы. !\n         :: #{@game.user.name}: #{@game.user.vieu_cards}
+         :: #{@game.assailant.name}: ☺, ☺ ?"
+    puts '            Ставки сделаны!'
+  end
+
+  def pause
+    puts "Продолжить: нажать 'Enter' "
+    gets
+  end
+
+  def command
+    puts 'Введите цифру'
+    gets.chomp
+  end
+
+  def wrong
+    puts 'Не правильный выбор команды'
+  end
+
+  def add_cards
+    puts "         -->>Ход: #{@game.user.name} -->> #{@game.user.view}-->>"
+    long_2
+  end
+
+  def long_1
+    sleep(1)
+  end
+
+  def long_2
+    sleep(2)
+  end
+
+  def long_3
+    sleep(3)
+  end
+
+  def error_message(mes)
+    puts "Erorr: #{mes}"
   end
 
   def validate!

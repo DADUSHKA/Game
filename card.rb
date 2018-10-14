2679a9c4e1e8b4c164868e7e5e4d1d6802b22da7
vicstor <vicstor@bk.ru>
2018-10-14 16:22:28 +0300

    over game new

--- card.rb
+++ card.rb
@@ -1,5 +1,9 @@
 class Card
-  CARD_VALUE = [10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+SUITS = %w[♦️ ♣️ ♠️ ♥️]
+ RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+
+  CARD_VALUE = [10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  CARD_SUIT = %w[♧ ♥ ♤ ♦]
   MAJOR_CARD = %w[V D K T]
 end
